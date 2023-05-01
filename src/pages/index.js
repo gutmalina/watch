6b7319b1e9b77af6e0fab147ac5a57b2360c2145
Marina Gutkevich <gutmalina@mail.ru>
2023-05-01 19:16:39 +0300

    make time

--- src/pages/index.js
+++ src/pages/index.js
@@ -105,5 +105,5 @@ handleQuote();
 
 let now = setInterval(() => {
   let date = new Date();
-  time.textContent = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  time.textContent = `${(''+date.getHours()).padStart(2, "0")}:${(''+date.getMinutes()).padStart(2, "0")}:${(''+date.getSeconds()).padStart(2, "0")}`;
 }, 1000);
